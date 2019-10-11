c2bc706b0ae9db1d447c8a782f0ce103840cb27e
bendou <sami.dahoux@etu.emse.fr>
2019-10-11 16:21:09 +0200

    updating bashdl

--- src/build_third.sh
+++
@@ -1,8 +0,0 @@
-export PROJECT_ROOT="."
-
-rm -rf $PROJECT_ROOT/lib/lib_thirdparty
-
-vlib $PROJECT_ROOT/lib/lib_thirdparty
-vmap lib_thirdparty $PROJECT_ROOT/lib/lib_thirdparty
-
-vcom -work lib_thirdparty $PROJECT_ROOT/lib_thirdpartylibrary/CryptPack.vhd
